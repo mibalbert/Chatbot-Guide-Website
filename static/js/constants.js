85d5b2c371a232e3b75a270a774ab19dc1fd5cf4
Alberto <mib.albert@gmail.com>
2022-04-16 06:11:06 +0100

    chnged ngrok

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "https://45ea-31-205-95-133.ngrok.io /webhooks/rest/webhook";
+const rasa_server_url = "https://45ea-31-205-95-133.ngrok.io/webhooks/rest/webhook";
 const sender_id = uuidv4();
