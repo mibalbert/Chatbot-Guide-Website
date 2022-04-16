e8237786b1979f8282b0ab081cb38113ca3eb128
Alberto <mib.albert@gmail.com>
2022-04-16 06:12:35 +0100

    sada

--- static/js/components/chat.js
+++ static/js/components/chat.js
@@ -300,7 +300,7 @@ function actionTrigger() {
 // eslint-disable-next-line no-unused-vars
 function customActionTrigger() {
     $.ajax({
-        url: "https://orange-bobcat-89.loca.lt/webhook/",
+        url: "https://tough-baboon-33.loca.lt/webhook/",
         type: "POST",
         contentType: "application/json",
         data: JSON.stringify({
