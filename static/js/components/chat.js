862098b8849ddbbce8c814ad032bcf4957cfc3dc
Alberto <mib.albert@gmail.com>
2022-04-16 11:21:02 +0100

    asidonasiodn

--- static/js/components/chat.js
+++ static/js/components/chat.js
@@ -300,7 +300,7 @@ function actionTrigger() {
 // eslint-disable-next-line no-unused-vars
 function customActionTrigger() {
     $.ajax({
-        url: "https://slimy-yak-94.loca.lt/webhook/",
+        url: "https://loud-dingo-12.loca.lt/webhook/",
         type: "POST",
         contentType: "application/json",
         data: JSON.stringify({
