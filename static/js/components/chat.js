85858450c0caf1cd798ac31f36f5a76e2acac4a3
Alberto <mib.albert@gmail.com>
2022-04-16 10:02:43 +0100

    Asdoai

--- static/js/components/chat.js
+++ static/js/components/chat.js
@@ -300,7 +300,7 @@ function actionTrigger() {
 // eslint-disable-next-line no-unused-vars
 function customActionTrigger() {
     $.ajax({
-        url: "https://tough-baboon-33.loca.lt/webhook/",
+        url: "https://slimy-yak-94.loca.lt/webhook/",
         type: "POST",
         contentType: "application/json",
         data: JSON.stringify({
