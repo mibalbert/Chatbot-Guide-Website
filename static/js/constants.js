5209950f695c4edc41c09e033e1d0d292aa2c1cd
Alberto <mib.albert@gmail.com>
2022-05-19 11:22:12 +0100

    ASDAsd

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "https://ec2-18-170-253-240.eu-west-2.compute.amazonaws.com:5005/webhooks/rest/webhook";
+const rasa_server_url = "http://ec2-18-170-253-240.eu-west-2.compute.amazonaws.com:5005/webhooks/rest/webhook";
 const sender_id = uuidv4();
