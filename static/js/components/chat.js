baaab04837e9ba59cfa89d148029bce96f6bca40
Alberto <mib.albert@gmail.com>
2022-05-23 20:19:10 +0100

    changed url for actions

--- static/js/components/chat.js
+++ static/js/components/chat.js
@@ -300,7 +300,7 @@ function actionTrigger() {
 // eslint-disable-next-line no-unused-vars
 function customActionTrigger() {
     $.ajax({
-        url: "http://ec2-18-170-253-240.eu-west-2.compute.amazonaws.com:5055/webhook",
+        url: "http://www.actionserverforpersonalprojects.com/webhook",
         type: "POST",
         contentType: "application/json",
         headers: {"Bypass-Tunnel-Reminder": "overpass"}, 
